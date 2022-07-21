b0cb6741f8eeb16f75dc48878481f23be6743cfe
Somya-Parihar <109022325+Somya-Parihar@users.noreply.github.com>
2022-07-21 22:52:21 +0530

    upload

---
+++ api.js
@@ -0,0 +1,18 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://dad-jokes.p.rapidapi.com/random/joke",
+	"method": "GET",
+	"headers": {
+		'X-RapidAPI-Key': 'b223f344e4msh676ae406cae4895p133982jsn00620b979240',
+		"X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+	console.log(response.body[0].setup)
+	var setup = response.body[0].setup;
+	var punchline = response.body[0].punchline;
+	$('#joke').append(setup+'<br>'+punchline);
+});
